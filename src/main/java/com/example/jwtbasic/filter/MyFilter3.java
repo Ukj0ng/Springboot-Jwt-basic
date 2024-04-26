71901f4e2994feb2338afcc83a073d3e0273f18f
Ukjong <xb253q@gmail.com>
2024-04-26 20:19:56 +0900

    post 요청의 header 토큰에 따른 servlet처리 테스트

--- src/main/java/com/example/jwtbasic/filter/MyFilter3.java
+++ src/main/java/com/example/jwtbasic/filter/MyFilter3.java
@@ -5,14 +5,36 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter3 implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
         throws IOException, ServletException {
-        System.out.println("필터3");
-        chain.doFilter(request, response);
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        // 토큰 : cos면 인증 완료, 아니면 controller로 진입도 차단, header의 Authorization에는 한글 못들어감 -> 영어로 쓰자
+        // 우리는 이 토큰을 만들어줘야함. id, pw가 정상적으로 들어오면 로그인이 완료되면 토큰을 만들어주고 그걸 응답 해준다.
+        // 요청할 때마다 header에 Authorization에 value값으로 토큰을 가지고 오겠지?
+        // 그때 토큰이 넘어오면 이 토큰이 내가 만든 토큰이 맞는지만 검증하면 됨. (RSA, HS256)
+        if (req.getMethod().equals("POST")) {
+            System.out.println("POST 요청됨");
+            System.out.println("필터3");
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println(headerAuth);
+
+            if (headerAuth.equals("cos")) {
+                chain.doFilter(req, res);
+            } else {
+                PrintWriter out = res.getWriter();
+                out.println("인증 안됨");
+            }
+        }
     }
 }
