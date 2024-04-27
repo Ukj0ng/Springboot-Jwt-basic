98cf820905993c76419461af80775e8e117cb5ed
Ukjong <xb253q@gmail.com>
2024-04-27 17:26:39 +0900

    feat: jpa repository 생성, query method 추가 - username으로 entity 찾기

--- src/main/java/com/example/jwtbasic/repository/UserRepository.java
+++ src/main/java/com/example/jwtbasic/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.example.jwtbasic.repository;public interface UserRepository {
+package com.example.jwtbasic.repository;
+
+import com.example.jwtbasic.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // findBy규칙 -> Username, Jpa query methods
+    // select * from user where username = 1?
+    User findByUsername(String username);
 }
