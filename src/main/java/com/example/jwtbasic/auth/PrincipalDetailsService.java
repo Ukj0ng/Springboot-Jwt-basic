aed4791cc2b5aa177ae23a983afc1afcf5374c6d
Ukjong <xb253q@gmail.com>
2024-04-27 17:23:26 +0900

    feat: UserDetail에 대한 service - 입력받은 username이 db에 있으면 해당 userEntity를 return

--- src/main/java/com/example/jwtbasic/auth/PrincipalDetailsService.java
+++ src/main/java/com/example/jwtbasic/auth/PrincipalDetailsService.java
@@ -1,2 +1,22 @@
-package com.example.jwtbasic.auth;public class PrincipalDetailsService {
+package com.example.jwtbasic.auth;
+
+import com.example.jwtbasic.model.User;
+import com.example.jwtbasic.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// http://localhost:8080/login 요청이 올 때 동작함. => SecurityConfig의 filterchain formlogin disable 때문에 동작을 안함
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+        return new PrincipalDetails(userEntity);
+    }
 }
