2c80152c328a0dde4b192ffb6f7b96b2263b5e9c
Ukjong <xb253q@gmail.com>
2024-04-27 17:27:51 +0900

    feat: AuthenticationManager 추가

--- src/main/java/com/example/jwtbasic/config/SecurityConfig.java
+++ src/main/java/com/example/jwtbasic/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.example.jwtbasic.config;
 
+import com.example.jwtbasic.auth.PrincipalDetailsService;
 import com.example.jwtbasic.filter.MyFilter3;
+import com.example.jwtbasic.jwt.JwtAuthenticationFilter;
+import com.example.jwtbasic.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
@@ -16,10 +23,32 @@ import org.springframework.web.filter.CorsFilter;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Autowired
+    private UserRepository userRepository;
+
     private final CorsFilter corsFilter;
     @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // AuthenticationManagerBuilder를 사용하여 AuthenticationManager를 설정하고 빌드합니다.
+    // AuthenticationManager 빈 직접 구성
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        // HttpSecurity의 AuthenticationManagerBuilder를 통해 구성
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(new PrincipalDetailsService(userRepository))
+            .passwordEncoder(bCryptPasswordEncoder());
+        return builder.build();
+    }
+
+
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception { // 이게 제일 먼저 한번 실행되나? 필터 순서가 이상해
+        AuthenticationManager authenticationManager = authenticationManager(http);
         http
+            .authenticationManager(authenticationManager)
             .addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class)
             .csrf(csrf -> csrf
                 .disable())
@@ -30,13 +59,13 @@ public class SecurityConfig {
                 .disable())     // form 태그 안씀
             .httpBasic(basic -> basic
                 .disable())     // http 통신 안씀
+            .addFilter(new JwtAuthenticationFilter(authenticationManager)) // AuthenticationManager를 매개변수로 담아줘야함.
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/user/**").hasAnyRole("USER", "MANAGER", "ADMIN")
                 .requestMatchers("/api/v1/manager/**").hasAnyRole("MANAGER", "ADMIN")
                 .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
             );
-
         return http.build();
     }
 }
